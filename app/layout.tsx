f034f20d7e2715fbedc1b1c861af395e9da9f829
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-01-29 16:35:52 -0800

    biome lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import "@/styles/globals.css"
 import type { Metadata, Viewport } from "next"
 import { Inter } from "next/font/google"
 
+import { ThemeProvider } from "@/components/theme-provider"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
-import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -70,7 +70,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={cn(
           "min-h-screen bg-background antialiased",
-          inter.className
+          inter.className,
         )}
       >
         <ThemeProvider
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center" />
+    </main>
+  )
+}
